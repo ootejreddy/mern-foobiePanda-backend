fbf74bddb332d600d28c74a3b4d7a2b0e60deccc
ootejreddy <mail2bsr007@gmail.com>
2024-06-09 17:52:30 -0500

    Completed backend of search, sort, filter functionality

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import MyUserRoute from "./routes/MyUserRoute";
 import { v2 as cloudinary } from "cloudinary";
 import MyRestaurantRoute from "./routes/MyRestaurantRoute";
+import RestaurantRoute from "./routes/RestaurantRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -27,6 +28,7 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 app.use("/api/my/user", MyUserRoute);
 app.use("/api/my/restaurant", MyRestaurantRoute);
+app.use("/api/restaurant", RestaurantRoute);
 
 app.listen(8000, () => {
   console.log("server started on localhost:8000");
