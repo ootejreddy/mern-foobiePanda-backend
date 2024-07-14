d8e08178091c0ce5c92a8dc1c20c47ec8821cece
ootejreddy <mail2bsr007@gmail.com>
2024-07-14 06:25:25 -0500

    Implemented function to get user roles from auth0

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -1,11 +1,21 @@
 import { Request, Response } from "express";
+import axios from "axios";
 import User from "../models/user";
+
+interface Role {
+  id: string;
+  name: string;
+  description: string;
+}
 
 const createCurrentUser = async (req: Request, res: Response) => {
   try {
     const { auth0Id } = req.body;
     console.log("The auth id is: ", auth0Id);
-
+    const accessToken = await getAccessToken();
+    getUserRoles(accessToken, auth0Id);
+    // getUserRoles(auth0Id, req.headers.authorization);
+    // getAccessToken();
     const existingUser = await User.findOne({ auth0Id });
     if (existingUser) {
       return res.status(200).send("User already exists");
@@ -18,6 +28,22 @@ const createCurrentUser = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Error Creating user" });
   }
 };
+
+// const getUserRoles = (auth0Id: string, authorization: any) => {
+//   var options = {
+//     method: "GET",
+//     url: `https://dev-b4nhd54dagy2jubp.us.auth0.com/api/v2/users/${auth0Id}/roles`,
+//     headers: { authorization: authorization },
+//   };
+//   axios
+//     .request(options)
+//     .then(function (response) {
+//       console.log("The roles data is: ", response.data);
+//     })
+//     .catch(function (error) {
+//       console.error(error);
+//     });
+// };
 
 const updateCurrentUser = async (req: Request, res: Response) => {
   try {
@@ -47,4 +73,37 @@ const getCurrentUser = async (req: Request, res: Response) => {
   return res.status(200).json(user);
 };
 
+// Function to get an access token
+async function getAccessToken(): Promise<string> {
+  const clientId = process.env.AUTH0_CLIENT_ID;
+  const clientSecret = process.env.AUTH0_CLIENT_SECRET;
+  const domain = process.env.AUTH0_DOMAIN;
+  const response = await axios.post(`https://${domain}/oauth/token`, {
+    client_id: clientId,
+    client_secret: clientSecret,
+    audience: `https://${domain}/api/v2/`,
+    grant_type: "client_credentials",
+  });
+  // console.log("The access token is: ", response.data.access_token);
+
+  return response.data.access_token;
+}
+
+async function getUserRoles(
+  accessToken: string,
+  userId: string
+): Promise<Role[]> {
+  const domain = process.env.AUTH0_DOMAIN;
+  const response = await axios.get(
+    `https://${domain}/api/v2/users/${userId}/roles`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+  console.log("The roles are: ", response.data);
+
+  return response.data;
+}
 export default { createCurrentUser, updateCurrentUser, getCurrentUser };
